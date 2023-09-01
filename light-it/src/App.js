748223f141a5b784263395fac77b8ec18695d46d
sbocaccio <sebastian.bocaccio@tiendanube.com>
2023-09-01 19:19:22 -0300

    Add sidebar

--- light-it/src/App.js
+++ light-it/src/App.js
@@ -1,38 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import myImage from './images/logo.jpeg';
-import { Select, Option } from "@material-tailwind/react";
-import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import {
+    Card,
+    Typography,
+    List,
+    ListItem,
+    ListItemPrefix,
+    ListItemSuffix,
+    Chip,
+} from "@material-tailwind/react";
+import {
+    PresentationChartBarIcon,
+    ShoppingBagIcon,
+    UserCircleIcon,
+    Cog6ToothIcon,
+    InboxIcon,
+    PowerIcon,
+} from "@heroicons/react/24/solid";
 
 
 function App() {
   return (
     <div className="App">
       <TopBar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Chupame la ponga <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+        <DefaultSidebar/>
+
+      {/*</header>*/}
     </div>
   );
 }
 
 const TopBar = () => {
+    const topBarStyle = {
+        backgroundColor: '#333333', // Set the background color to #333333
+        color: 'white', // Set the text color to white or any desired color
+
+    };
+
     return (
-        <div className="bg-gray-800 text-white py-2">
-            <div className="container mx-auto py-2 flex justify-between items-center">
+        <div className="bg-gray-800 text-white py-2" style={topBarStyle}>
+            <div className="container mx-auto py-2 flex justify-between items-center" >
                 <img src={myImage} alt="My Image" className="max-w-xs h-auto" />
                 <SaveDropDown/>
             </div>
@@ -102,6 +113,35 @@ function SaveDropDown() {
                 </Menu.Items>
         </Menu>
     )
+}
+
+
+function DefaultSidebar() {
+    return (
+        <Card className="h-[calc(100vh-2rem)] w-full max-w-[8rem] p-4 shadow-xl shadow-blue-gray-900/5">
+            <List className="flex flex-col justify-center h-full"> {/* Use flex to center and expand items vertically */}
+                <ListItem className="flex flex-col items-center"> {/* Use flex to center the items */}
+                    <ListItemPrefix>
+                        <PresentationChartBarIcon className="h-5 w-5" />
+                    </ListItemPrefix>
+                    Dashboard
+                </ListItem>
+                <ListItem className="flex flex-col items-center"> {/* Use flex to center the items */}
+                    <ListItemPrefix>
+                        <ShoppingBagIcon className="h-5 w-5" />
+                    </ListItemPrefix>
+                    E-Commerce
+                </ListItem>
+                <ListItem className="flex flex-col items-center"> {/* Use flex to center the items */}
+                    <ListItemPrefix>
+                        <InboxIcon className="h-5 w-5" />
+                    </ListItemPrefix>
+                    Inbox
+                </ListItem>
+                {/* Repeat this pattern for the rest of your list items */}
+            </List>
+        </Card>
+    );
 }
 
 export default App;
