71f3be7f8800e48895526b1ee3b0b76795e5482f
sbocaccio <sebastian.bocaccio@tiendanube.com>
2023-09-02 19:43:46 -0300

    Menu items now are dinamicaly created

--- light-it/src/App.js
+++ light-it/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import MainPage from "./components/MainPage/MainPage";
+import Client from "./utils/Client";
 
 function App() {
   return (
--- light-it/src/components/SideBar/SideBarMenu.jsx
+++ light-it/src/components/SideBar/SideBarMenu.jsx
@@ -2,8 +2,14 @@ import {Card, List, ListItem, ListItemPrefix} from "@material-tailwind/react";
 import aberturas from "../../images/Aberturas.png";
 import equipamiento from "../../images/Equipamiento.png";
 import terminaciones from "../../images/Terminaciones.png";
+import Client from "../../utils/Client";
 
 export default function SideBarMenu() {
+
+    const client = new Client()
+    const aberturasContent = client.fetchAberturas();
+    const menuItems = [{image: aberturas, text: 'Aberturas'}, {image: equipamiento, text: 'Equipamiento' }, {image: terminaciones, text: 'Terminaciones'}]
+
     const fontStyle = {
         'font-size': '0.60rem',
         'line-height': '1rem'
@@ -11,25 +17,16 @@ export default function SideBarMenu() {
     return (
         <Card className="h-[calc(100vh-2rem)] w-full max-w-[6rem] p-4 shadow-xl shadow-blue-gray-900/5">
             <List className="flex flex-col justify-center h-full">
-                <ListItem className="flex flex-col items-center">
-                    <ListItemPrefix>
-                        <img src={aberturas} alt="My Image" className="h-6 w-6" />
-                    </ListItemPrefix>
-                    <div className="text-xs" style={fontStyle}>Aberturas</div>
-                </ListItem>
-                <ListItem className="flex flex-col items-center">
-                    <ListItemPrefix>
-                        <img src={equipamiento} alt="My Image" className="h-6 w-6" />
-                    </ListItemPrefix>
-                    <div className="text-xs" style={fontStyle}>Equipamiento</div>
-
-                </ListItem>
-                <ListItem className="flex flex-col items-center ">
-                    <ListItemPrefix>
-                        <img src={terminaciones} alt="My Image" className="h-6 w-6" />
-                    </ListItemPrefix>
-                    <div className="text-xs" style={fontStyle}>Terminaciones</div>
-                </ListItem>
+                {menuItems.map((menuItem, index) => (
+                    <ListItem key={index} className="flex flex-col items-center">
+                        <ListItemPrefix>
+                            <img src={menuItem.image} className="h-6 w-6"/>
+                        </ListItemPrefix>
+                        <div className="text-xs" style={fontStyle}>
+                            {menuItem.text}
+                        </div>
+                    </ListItem>
+                ))}
             </List>
         </Card>
     )
