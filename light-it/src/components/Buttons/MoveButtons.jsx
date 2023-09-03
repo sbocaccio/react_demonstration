74a98eff15b9880ab55e058f209b6415f103d925
sbocaccio <sebastian.bocaccio@tiendanube.com>
2023-09-03 17:07:14 -0300

    Agrego comportamiento MoveButton

--- light-it/src/components/Buttons/MoveButtons.jsx
+++ light-it/src/components/Buttons/MoveButtons.jsx
@@ -1,31 +1,37 @@
-import myImage from "../../images/logo.jpeg";
+import Flechas from "../../images/4Flechas.png";
 
 export default function MoveButtons() {
     const buttonsStyle = {
-        width: '24px',
-        height: '24px',
+        width: '50px',
+        height: '50px',
         padding: '0px'
 
     }
+    const handleButtonClick = (event) => {
+        const buttonRect = event.target.getBoundingClientRect();
+        const x = event.clientX - buttonRect.left;
+        const y = event.clientY - buttonRect.top;
+        const buttonWidth = buttonRect.width;
+        const buttonHeight = buttonRect.height;
+        const windowWidth = window.innerWidth;
+
+        if (x < buttonWidth / 2 && y < buttonHeight / 3) {
+            console.log('Clickeaste Arriba');
+        } else if (x > buttonWidth / 2 && y <= (2 / 3) * buttonHeight) {
+            console.log('Clickeaste Derecha');
+        } else if (x <= buttonWidth / 2 && y >= buttonHeight / 3 && y <= (2 / 3) * buttonHeight && x <= windowWidth / 2) {
+            console.log('Clickeaste Izquierda');
+        } else{
+            console.log('Clickeaste Abajo');
+        }
+    };
+
     return (
         <div className="">
             <div className="absolute right-0  p-4" style={{ bottom: '-60px' }}>
-                <div className="arrow-button-row" style={{ marginBottom: '-10px', justifyContent: 'space-between'}}>
-                    <button className="arrow-button">
-                        <img src={myImage} alt="Up Arrow" style={buttonsStyle} />
+                    <button className="arrow-button" onClick={handleButtonClick}>
+                        <img src={Flechas} alt="Left Arrow" style={buttonsStyle} />
                     </button>
-                    <button className="arrow-button">
-                        <img src={myImage} alt="Down Arrow" style={buttonsStyle} />
-                    </button>
-                </div>
-                <div className="arrow-button-row" style={{ marginBottom: '0', justifyContent: 'space-between',  }}>
-                    <button className="arrow-button">
-                        <img src={myImage} alt="Left Arrow" style={buttonsStyle} />
-                    </button>
-                    <button className="arrow-button">
-                        <img src={myImage} alt="Right Arrow" style={buttonsStyle} />
-                    </button>
-                </div>
             </div>
         </div>
     );
